f9d4a5fde2c2bc005de93ac7095ed0e320eb4c0a
Konstantina <77291262+ksokratous@users.noreply.github.com>
2026-03-13 21:16:01 -0500

    changes

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const redirectedPath = new URLSearchParams(window.location.search).get("p");
+
+if (redirectedPath) {
+  window.history.replaceState(null, "", redirectedPath);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
