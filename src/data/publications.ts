27585550e9bda0135766dea275543fecc1be50c3
Konstantina <77291262+ksokratous@users.noreply.github.com>
2026-09-22 13:30:05 -0500

    update publications.ts

--- src/data/publications.ts
+++ src/data/publications.ts
@@ -7,24 +7,25 @@ export interface Publication {
 }
 
 export const publications: Publication[] = [
-   {
+    {
     year: "2026",
-    journal: "",
-    title: "Machine Psychometrics for Large Language Models",
-    authors: "Sokratous, K., Vandekerckhove, J., & Davis-Stober, C.",
-    link: "#",
-  },
-
-  {
-    year: "2026 (In Press)",
     journal: "Journal of Choice Modeling",
     title: "Time flips and price dips: Modeling preference reversals between intertemporal choice and pricing",
     authors: "Sokratous K. & Kvam, P. D.",
     link: "https://doi.org/10.31219/osf.io/vhgm8",
   },
+
+   {
+    year: "2026",
+    journal: "",
+    title: "Machine Psychometrics for Large Language Models",
+    authors: "Sokratous, K., Vandekerckhove, J., & Davis-Stober, C.",
+    link: "https://osf.io/preprints/psyarxiv/up3ye_v1",
+  },
+
   {
     year: "2026",
-    journal: "Cognition (Under Review)",
+    journal: "Cognition",
     title: "The Timing of Suboptimality: How Temporal Uncertainty Drives Procrastination Behavior",
     authors: "Sokratous, K., Hintze, A., Fitch, A., & Kvam, P. D.,",
     link: "https://osf.io/preprints/osf/trkv9_v3",
